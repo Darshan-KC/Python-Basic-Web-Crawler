db3cf60e7d1868bfcc5774d0c82fd5ed36ef5eeb
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-01-09 18:58:16 +0545

    extract links added

--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
 # Helper functions (e.g., URL validation, HTML parsing)
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+
+def extract_links(html, base_url):
+    soup = BeautifulSoup(html, 'html.parser')
+    links = set()
+    for tag in soup.find_all('a', href=True):
+        full_url = urljoin(base_url, tag['href'])
+        links.add(full_url)
+    return links
+
